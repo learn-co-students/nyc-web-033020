b719eab41df4fb5593356bae99c1f67a0f43a3c4
Dwire <gjdwyer8@gmail.com>
2020-04-23 12:31:40 -0400

    done

---
+++ 13-intro-to-rails/corn_school/app/models/student.rb
@@ -0,0 +1,5 @@
+class Student < ActiveRecord::Base
+
+
+    
+end
---
+++ 13-intro-to-rails/rails_school/app/controllers/students_controller.rb
@@ -0,0 +1,78 @@
+class StudentsController < ApplicationController
+  before_action :set_student, only: [:show, :edit, :update, :destroy]
+
+  # GET /students do 
+  # end 
+  def index
+    @students = Student.all
+    # render :index
+  end
+ 
+  # GET /students/:id
+  # GET /students/1.json
+  def show
+    @student = Student.find(params[:id])
+  end
+
+  # GET /students/new
+  def new
+    @student = Student.new
+  end
+
+  # GET /students/1/edit
+  def edit
+    @student = Student.find(params[:id])
+  end
+
+  # POST /students
+  # POST /students.json
+  def create
+    @student = Student.new(student_params)
+
+    respond_to do |format|
+      if @student.save
+        format.html { redirect_to @student, notice: 'Student was successfully created.' }
+        format.json { render :show, status: :created, location: @student }
+      else
+        format.html { render :new }
+        format.json { render json: @student.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /students/:id
+  # PATCH/PUT /students/1.json
+  def update
+    @student = Student.find(params[:id])
+    respond_to do |format|
+      if @student.update(student_params)
+        format.html { redirect_to @student, notice: 'Student was successfully updated.' }
+        format.json { render :show, status: :ok, location: @student }
+      else
+        format.html { render :edit }
+        format.json { render json: @student.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /students/1
+  # DELETE /students/1.json
+  def destroy
+    @student.destroy
+    respond_to do |format|
+      format.html { redirect_to students_url, notice: 'Student was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_student
+      @student = Student.find(params[:id])
+    end
+
+    # Only allow a list of trusted parameters through.
+    def student_params
+      params.require(:student).permit(:name, :grade)
+    end
+end
