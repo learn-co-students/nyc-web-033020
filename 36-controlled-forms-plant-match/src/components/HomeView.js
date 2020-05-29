a5b78db7785226d9716b81460125853d5232f907
Caryn McCarthy <cmccarthy15@gmail.com>
2020-05-29 17:20:25 -0400

    starter code posted

---
+++ 36-controlled-forms-plant-match/src/components/HomeView.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import MatchContainer from './MatchContainer';
+import CreatePlantForm from './CreatePlantForm';
+
+const API_BASE = 'http://localhost:3001/plants'
+
+class HomeView extends React.Component {
+    state = {
+        plants: [],
+        showCreateForm: false,
+    }
+
+    /** DO NOT WORRY ABOUT THIS COMPONENTDIDMOUNT METHOD
+     * It's part of content we'll learn on FRIDAY of WEEK ONE
+    */
+    componentDidMount(){
+        fetch(API_BASE)
+            .then(res => res.json())
+            .then(plants => this.setState({ plants }))
+    }
+
+    toggleCreateForm = () => this.setState({ showCreateForm: !this.state.showCreateForm })
+
+
+    /**
+     * TODO: ONLY FOR ADVANCED DELIVERABLES
+     * Once you post a new plant, you'll need to update the plants on state here.
+     * Define a method that can add a new plant into the plants array.
+     */
+
+    render(){
+        const { plants, showCreateForm } = this.state
+        // TODO: In order to search, what state, methods and element attributes are needed? 
+        // In order to render the correct plants, what calculations do you need to do and what props do you need to change below?
+
+        return (
+            <div>
+                <button onClick={this.toggleCreateForm}>{showCreateForm ? "Hide Form" : "Submit Plant"}</button>
+                { showCreateForm && <CreatePlantForm />}
+                <hr />
+                <div>
+                    <input placeholder="Search for Plants"/>
+                </div>
+                <MatchContainer plants={plants}/>
+            </div>
+        )
+    }
+}
+
+export default HomeView;
