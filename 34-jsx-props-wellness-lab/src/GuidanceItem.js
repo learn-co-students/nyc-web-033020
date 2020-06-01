b3091fd886183b5a92d6a18d298d24e625e73c4f
github username <github email address>
2020-06-01 13:07:52 -0400

    ex34

--- 34-jsx-props-wellness-lab/src/GuidanceItem.js
+++ 34-jsx-props-wellness-lab/src/GuidanceItem.js
@@ -1,14 +1,18 @@
 import React from 'react';
 
-const GuidanceItem = () => {
-    return (
-        <tr>
-            <td>{props.factor.title}</td>
-            <td><img src={props.factor.image} alt={props.factor.title}/></td>
-            <td>{/** TODO: Add the correct prop here */}</td>
-            <td>{/** TODO: Add the correct prop here */}</td>
-        </tr>
-    )
+class GuidanceItem extends React.Component {
+	render (){
+		const {title, image, description, stars, isPriority} = this.props;
+    	return (
+        	<tr>
+            	<td>{title}</td>
+            	<td><img src={image} alt={title}/></td>
+            	<td>{description}</td>
+            	<td>{'⭐️'.repeat(stars)}</td>
+            	<td>{isPriority}</td>
+        	</tr>
+    	)
+    }
 }
 
 export default GuidanceItem;
--- 34-jsx-props-wellness-lab/src/Wheel.js
+++ 34-jsx-props-wellness-lab/src/Wheel.js
@@ -8,10 +8,11 @@ export default class Wheel extends React.Component {
     render(){
         return (
             <Pie size={300}>
-                {/** TODO: map over your wellness factors and create a WellnessItem for each. 
-                 *         check the WellnessItem component to see what it's expecting for props
-                 *         Hint: Use your ES6 techniques
-                 */}
+            	{
+					wellnessFactors.map(factor => {
+						return <WellnessItem id={factor.id} color={factor.color} title={factor.title} isPriority={factor.isPriority} />
+				})
+            	}
             </Pie>
         )
     }
