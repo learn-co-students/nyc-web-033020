d065937dcb93884262023ad11d58dd387bb95ef7
Caryn McCarthy <cmccarthy15@gmail.com>
2020-06-01 15:24:22 -0400

    after props lecture

--- 34-jsx-props-wellness-lab/src/Wheel.js
+++ 34-jsx-props-wellness-lab/src/Wheel.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { wellnessFactors } from './data';
+import { wellnessFactors } from './data'; // aliases
 import WellnessItem from './WellnessItem';
 import { Pie } from './styles';
 
@@ -8,7 +8,8 @@ export default class Wheel extends React.Component {
     render(){
         return (
             <Pie size={300}>
-                {/** TODO: map over your wellness factors and create a WellnessItem for each. 
+                { wellnessFactors.map(factor => <WellnessItem key={factor.id} {...factor}  />)
+                /** TODO: map over your wellness factors and create a WellnessItem for each. 
                  *         check the WellnessItem component to see what it's expecting for props
                  *         Hint: Use your ES6 techniques
                  */}
@@ -16,3 +17,5 @@ export default class Wheel extends React.Component {
         )
     }
 }
+
+// export const getFetch = () => fetch('localhost:3000/songs').then(res => res.json())
