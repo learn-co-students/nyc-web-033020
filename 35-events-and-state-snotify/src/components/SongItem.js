2cdce1ecb3be86f6ee29f811dbf5379e2dff307f
jnzhang622 <jnzhang622@gmail.com>
2020-06-02 10:34:31 -0400

    pre lecture commit

--- 35-events-and-state-snotify/src/components/SongItem.js
+++ 35-events-and-state-snotify/src/components/SongItem.js
@@ -1,16 +1,19 @@
 import React from 'react';
 
-const SongItem = props => {
-    return (
-        <tr>
-            <td>{props.title}</td>
-            <td>{props.artist}</td>
-            <td><button onClick={null /* Put your click handler here */}>Play Now</button></td>
-            <td><button onClick={null /* Put your click handler here */}>Add to Queue</button></td>
-            <td onClick={null /* Put your click handler here */}>{props.favorite ? "💚" : "♡"}</td>
-            <td>Likes: 0</td>
-        </tr>
-    )
+class SongItem extends React.Component {
+    render() {
+        const { title, artist } = this.props;
+        return (
+            <tr>
+                <td>{props.title}</td>
+                <td>{props.artist}</td>
+                <td><button onClick={null /* Put your click handler here */}>Play Now</button></td>
+                <td><button onClick={null /* Put your click handler here */}>Add to Queue</button></td>
+                <td onClick={null /* Put your click handler here */}>{props.favorite ? "💚" : "♡"}</td>
+                <td>Likes: 0</td>
+            </tr>
+        )
+    }
 }
 
 export default SongItem;
