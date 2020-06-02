539eed1c31762b23da2b9451bd539b74413888cc
theluispestana <theluispestana@gmail.com>
2020-06-02 10:35:01 -0400

    working on first deliverable

--- 35-events-and-state-snotify/src/App.js
+++ 35-events-and-state-snotify/src/App.js
@@ -1,29 +1,38 @@
-import React from 'react';
-import './App.css';
-import MainContainer from './components/MainContainer';
+import React from "react";
+import "./App.css";
+import MainContainer from "./components/MainContainer";
 
-let API_ENDPOINT = `http://localhost:6001/songs`
+let API_ENDPOINT = `http://localhost:6001/songs`;
 
 class App extends React.Component {
   state = {
     /* TODO: What should go in state here?? Anything we don't want to have to fetch again for instance...? */
-  }
-  
+    songs: null,
+  };
+
+  fetchSong = () => {
+    fetch("http://localhost:6002/songs")
+      .then((res) => res.json())
+      .then((json) => this.setState({ songs: json }));
+  };
+
   renderNav = () => {
     return (
       <div className="simple-flex-row">
-        <button onClick={null /* TODO: Put your method to fetch the songs */}>Get Songs</button> 
+        <button onClick={this.fetchSong}>Get Songs</button>
         <h1>S-not-ify 🐽</h1>
-        <input placeholder="Search by title or artist..."/>
+        <input placeholder="Search by title or artist..." />
       </div>
-    )
-  }
+    );
+  };
 
-  render(){
+  render() {
     return (
       <div className="App">
-        {this.renderNav()} {/** The renderNav method renders a div holding the button to get songs and the title */}
-        <MainContainer /> {/** TODO: What props do I need? */}
+        {this.renderNav()}{" "}
+        {/** The renderNav method renders a div holding the button to get songs and the title */}
+        <MainContainer songs={this.state.songs} />
+        {/** TODO: What props do I need? */}
       </div>
     );
   }
--- 35-events-and-state-snotify/src/components/MainContainer.js
+++ 35-events-and-state-snotify/src/components/MainContainer.js
@@ -1,14 +1,15 @@
-import React from 'react';
-import SongList from './SongList';
-import Queue from './Queue';
+import React from "react";
+import SongList from "./SongList";
+import Queue from "./Queue";
 
-const MainContainer = props => {
-    return (
-        <div className="simple-flex-row top">
-            <SongList /> {/** TODO: What props do I need? */}
-            <Queue /> {/** TODO: What props do I need? */}
-        </div>
-    )
-}
+const MainContainer = (props) => {
+  return (
+    <div className="simple-flex-row top">
+      <SongList songs={props.songs} /> {/** TODO: What props do I need? */}
+      <Queue /> {/** TODO: What props do I need? */}
+    </div>
+  );
+};
 
 export default MainContainer;
+
