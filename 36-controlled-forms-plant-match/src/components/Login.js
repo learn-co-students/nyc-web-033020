57765f1f2741b958889e9ce9a2c08188803b26f6
elliott-king <8884571+elliott-king@users.noreply.github.com>
2020-06-03 14:22:10 -0400

    Correction to include value in render

--- 36-controlled-forms-plant-match/src/components/Login.js
+++ 36-controlled-forms-plant-match/src/components/Login.js
@@ -2,7 +2,10 @@ import React from 'react';
 
 class Login extends React.Component {
     state = {
-        // TODO: What needs to be represented in state for a fully controlled form?
+        name: "",
+        username: "",
+        password: "",
+        confirm: "",
     }
     // TODO: What methods need to be created for a fully controlled form?
     // HINT: Use the line below to change the view when the form is submitted
@@ -15,17 +18,18 @@ class Login extends React.Component {
 
     onSubmit = (event) => {
         this.props.changeView('home')
+        if (this.state.username) this.props.changeUsername(this.state.username)
     }
 
     render(){
         // TODO: What additional attributes and event handlers are needed on each of the elements below?
         return (
-            <form className="vertical-flex" onChange={this.onChange} onSubmit={this.onSubmit}>
+            <form className="vertical-flex" onSubmit={this.onSubmit}>
                 <h2>Create an Account</h2>
-                <input name="name" placeholder="Name"/>
-                <input name="username" placeholder="Username"/>
-                <input name="password" placeholder="Password"/>
-                <input name="confirm" placeholder="Confirm Password"/>
+                <input name="name" placeholder="Name" value={this.state.name} onChange={this.onChange}/>
+                <input name="username" placeholder="Username" value={this.state.username} onChange={this.onChange}/>
+                <input name="password" placeholder="Password" value={this.state.password} onChange={this.onChange}/>
+                <input name="confirm" placeholder="Confirm Password" value={this.state.confirm} onChange={this.onChange}/>
                 <button type="submit">Submit</button>
             </form>
         )
